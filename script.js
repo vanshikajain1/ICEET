9855f97dbd15683e70cf56bcdbe91a0f193e92eb
Vanshika Jain <jainvanshika0102@gmail.com>
2022-11-04 00:40:53 +0530

    final

--- script.js
+++ script.js
@@ -2,39 +2,36 @@
 var countDownDate = new Date("Febuary 28, 2023 03:45:40").getTime();
 
 // Update the count down every 1 second
-var x = setInterval(function() {
-
+var x = setInterval(function () {
   // Get today's date and time
   var now = new Date().getTime();
-    
+
   // Find the distance between now and the count down date
   var distance = countDownDate - now;
-    
+
   // Time calculations for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-//   // add in string time
-//   let time ='§{davs} : S{hours} : s{minutes} : ${seconds}';
-// // Output the result in an element
-//   document.querySelector('#countdown').innerText=time;
+
+  //   // add in string time
+  //   let time ='§{davs} : S{hours} : s{minutes} : ${seconds}';
+  // // Output the result in an element
+  //   document.querySelector('#countdown').innerText=time;
 
   // Output the result in an element with id="demo"
-  document.getElementById("timer1").innerHTML = days + " D " ;
+  document.getElementById("timer1").innerHTML = days + " D ";
   document.getElementById("timer2").innerHTML = hours + " H ";
-  document.getElementById("timer3").innerHTML = minutes + " M " ;
+  document.getElementById("timer3").innerHTML = minutes + " M ";
   document.getElementById("timer4").innerHTML = seconds + " S ";
 
-
-  // If the count down is over, write some text 
+  // If the count down is over, write some text
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("countdown").innerHTML = "SORRY, YOU MISSED IT :(";
   }
 }, 1000);
-
 
 // slider
 // var slideIndex = 1;
@@ -52,16 +49,24 @@ var x = setInterval(function() {
 //   var i;
 //   var slides = document.getElementsByClassName("mySlides");
 //   var dots = document.getElementsByClassName("dot");
-//   if (n > slides.length) {slideIndex = 1}    
+//   if (n > slides.length) {slideIndex = 1}
 //   if (n < 1) {slideIndex = slides.length}
 //   for (i = 0; i < slides.length; i++) {
-//       slides[i].style.display = "none";  
+//       slides[i].style.display = "none";
 //   }
 //   for (i = 0; i < dots.length; i++) {
 //       dots[i].className = dots[i].className.replace(" active", "");
 //   }
-//   slides[slideIndex-1].style.display = "block";  
+//   slides[slideIndex-1].style.display = "block";
 //   dots[slideIndex-1].className += " active";
 // }
 
+const express = require("express"); //express library ka content express constant me daldo
+const http = require("http");
+const app = express(); //initialized and server ready
 
+app.use(express.static("front"));
+let port = process.env.PORT || 3000; // end point for to and fro movement of dataflow
+let server = app.listen(port, () => {
+  console.log("Listning to port " + port);
+});
