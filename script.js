0bd9252e2a5351e757b6b1ca5cd309a494422b94
Vanshika Jain <jainvanshika0102@gmail.com>
2022-11-04 13:56:53 +0530

    new

--- script.js
+++ script.js
@@ -70,8 +70,3 @@ var x = setInterval(function () {
 // let server = app.listen(port, () => {
 //   console.log("Listning to port " + port);
 // });
-if (typeof window !== 'undefined') {
-  console.log('You are on the browser')
-} else {
-  console.log('You are on the server')
-}
